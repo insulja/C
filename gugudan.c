51fff4323436fa5ef1c1849fc23a7b2f67ddb7b2
Justin Hwang <justinh1217@gmail.com>
2017-02-07 23:06:25 +0900

    "While" changed to "for"

--- gugudan.c
+++ gugudan.c
@@ -2,16 +2,21 @@
 int multi();
 
 int main(void) {
-    int i = 0, j = 1;
+    int i = 0, j = 1, cnt;
     printf("Enter Gugudan Number : ");
     scanf("%d", &i);
 
-    while(j < 10) {
+    for(cnt = 0; cnt < 9; cnt++) {
         printf("%d X %d = %d\n", i, j, multi(i, j));
         j++;
     }
+
+    // while(j < 10) {
+    //     printf("%d X %d = %d\n", i, j, multi(i, j));
+    //    j++;
     return 0;
 }
+
     int multi(i, j) {
     return i * j;
     }
